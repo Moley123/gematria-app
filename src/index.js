ad5c5693cc0250ece6390be9a4bb27720a81077e
Moishie Lebrett <moishie.lebrett123@gmail.com>
2025-12-22 14:57:34 +0100

    Routing fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 
-console.log("Sanity Check: index.js is running!");
+console.log('Sanity Check: index.js is running and mounting App');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div style={{ backgroundColor: 'red', color: 'white', padding: '50px', fontSize: '30px' }}>
-    <h1>SYSTEM STATUS: ONLINE</h1>
-    <p>If you see this, React is working.</p>
-    <p>The issue is inside App.js or LandingPage.js</p>
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
